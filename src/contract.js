d23b48a894f19bcac8657a20510b2198eabd0929
khite-engineering <72549011+khite-engineering@users.noreply.github.com>
2022-02-15 14:45:28 +0100

    Update contract.js

--- src/contract.js
+++ src/contract.js
@@ -5,7 +5,7 @@ document.getElementById("btn-login").onclick = getTransactions;
 
 export const getTransactions = async () =>{
     console.log(await Web3.eth.getAccounts())
-    const Contract = Web3.eth.Contract(BrowntokenABI.abi, "0x62316331d7A024E56966baC81800D69CdDD62362", {from: '0x3CA0FdaC79A84138dfC00DaA3Dfa1AfE0752EE4a', gas:5500000 } )
+    const Contract = Web3.eth.Contract(BrowntokenABI.abi, "walletAddress", {from: '0x3CA0FdaC79A84138dfC00DaA3Dfa1AfE0752EE4a', gas:5500000 } )
     console.log(Contract)
 
 }
