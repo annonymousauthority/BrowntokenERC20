c36754720fd7a1d733f47870f9a5159767a86405
khite-engineering <72549011+khite-engineering@users.noreply.github.com>
2022-02-15 14:45:44 +0100

    Update contract.js

--- src/contract.js
+++ src/contract.js
@@ -1,11 +1,10 @@
 import { Web3 } from "web3";
-const BrowntokenABI = require('./build/contracts/Browntoken.json')
 
 document.getElementById("btn-login").onclick = getTransactions;
 
 export const getTransactions = async () =>{
     console.log(await Web3.eth.getAccounts())
-    const Contract = Web3.eth.Contract(BrowntokenABI.abi, "walletAddress", {from: '0x3CA0FdaC79A84138dfC00DaA3Dfa1AfE0752EE4a', gas:5500000 } )
+    const Contract = Web3.eth.Contract(token.abi, "walletAddress", {from: '0x3CA0FdaC79A84138dfC00DaA3Dfa1AfE0752EE4a', gas:5500000 } )
     console.log(Contract)
 
 }
